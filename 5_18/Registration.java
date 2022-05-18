dd715e224cbf34a5971844b4ca1ea1d54170a2f8
jinlk <45725145+jinyulink@users.noreply.github.com>
2022-05-18 15:19:51 +0800

    Delete 5_18 directory

--- 5_18/Registration.java
+++
@@ -1,9 +0,0 @@
-public class Registration {
-    public static void main(String[] args){
-        UnderGraduate u1 = new UnderGraduate("Mary","MIS");
-        u1.addCredits(21);
-        u1.addCredits(15);
-        u1.setHonor(true);
-        System.out.println(u1.tuition());
-    }
-}
